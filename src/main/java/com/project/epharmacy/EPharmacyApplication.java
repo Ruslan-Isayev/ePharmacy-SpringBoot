34c00a8fbb8d23aaf11788b84a396a45f33a80a2
Ruslan Isayev <70106358+Ruslan-Isayev@users.noreply.github.com>
2023-05-08 18:59:30 +0400

    Add files via upload

---
+++ src/main/java/com/project/epharmacy/EPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.project.epharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EPharmacyApplication.class, args);
+    }
+
+}
